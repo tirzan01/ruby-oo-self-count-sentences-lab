050404ceffb7fe216e04862d71a498d7c536e169
thomas <thomasmonta98@gmail.com>
2021-08-30 14:16:23 +1000

    done with assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    split_sentence = [self]
+    split_sentence = split_sentence.map{ |sentence| sentence.split('.') }.flatten
+    split_sentence = split_sentence.map{ |sentence| sentence.split('!') }.flatten
+    split_sentence = split_sentence.map{ |sentence| sentence.split('?') }.flatten
+    split_sentence.reject { |word| word.length == 0 }
+    split_sentence.length
   end
 end
